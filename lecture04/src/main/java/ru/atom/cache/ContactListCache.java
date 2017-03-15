4f7fe381071e95d7fcf49e6d75c4f8dc947b1a4b
nikfuri <nikfurirus@gmail.com>
2017-03-15 18:36:25 +0300

    lec 4

--- lecture04/src/main/java/ru/atom/cache/ContactListCache.java
+++ lecture04/src/main/java/ru/atom/cache/ContactListCache.java
@@ -2,33 +2,48 @@ package ru.atom.cache;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * What about Map?
  */
 public class ContactListCache extends AbstractCache<Person, List<? extends Person>> {
+    private final Map<Person, List<? extends Person>> cache = new HashMap<>(capacity);
+
     public ContactListCache(int capacity) {
         super(capacity);
     }
 
     @Override
     public boolean put(Person person, List<? extends Person> people) {
-        throw new NotImplementedException();
+        if (cache.size() == capacity && !cache.containsKey(person)) {
+            removeAny();
+        }
+        cache.put(person, people);
+        return true;
     }
+
 
     @Override
     public List<? extends Person> get(Person person) {
-        throw new NotImplementedException();
+        return cache.get(person);
     }
 
     @Override
     public int getSize() {
-        throw new NotImplementedException();
+        return cache.size();
     }
 
     private boolean removeAny() {
-        throw new NotImplementedException();
+        Person remove = null;
+        for (Person key : cache.keySet()) {
+            remove = key;
+            break;
+        }
+        cache.remove(remove);
+        return true;
     }
 
 }
--- lecture04/src/main/java/ru/atom/cache/Person.java
+++ lecture04/src/main/java/ru/atom/cache/Person.java
@@ -21,4 +21,22 @@ public class Person {
     public String getFamilyName() {
         return familyName;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (firstName != null ? !firstName.equals(person.firstName) : person.firstName != null) return false;
+        return familyName != null ? familyName.equals(person.familyName) : person.familyName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName != null ? firstName.hashCode() : 0;
+        result = 31 * result + (familyName != null ? familyName.hashCode() : 0);
+        return result;
+    }
 }
